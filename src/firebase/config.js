9da5acc8b4ab7fbeccdc1000133637a5e48dc9e9
Ritik6233 <90018013+Ritik6233@users.noreply.github.com>
2026-04-20 11:45:11 +0530

    add env config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,15 @@
 // ✅ REPLACE the values below with YOUR Firebase config from Step 3
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+ 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsVFoyEcgXIOhfkIkOWCp5e7t3nUsSA1M",
-  authDomain: "pama-web-9b175.firebaseapp.com",
-  databaseURL: "https://pama-web-9b175-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pama-web-9b175",
-  storageBucket: "pama-web-9b175.firebasestorage.app",
-  messagingSenderId: "629809125523",
-  appId: "1:629809125523:web:3ee941b777ec97d5535566"
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
+ 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
